6c7febe8da19fe552d21f5e7a7ba234437729d79
just-Qurri <zahar_sytko_r@mail.ru>
2026-03-31 15:34:23 +0300

    Fixed SPAC810T calculations for HV and LV sides

--- models/spac810T.py
+++ models/spac810T.py
@@ -27,39 +27,39 @@ class SPAC810TDevice(ProtectionDevice):
             return {
                 'retom_hv1': round((2*I_brake1+I_diff)/2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
                 'retom_lv1': round((2*I_brake1-I_diff)/2 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
-                'retom_skvoz_hv1' : round(I_brake1 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
-                'retom_skvoz_lv1' : round(I_brake1 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
+                'retom_skvoz_hv1' : round((2*I_brake1+I_diff)/2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
+                'retom_skvoz_lv1' : round((2*I_brake1+I_diff)/2 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
                 'retom_hv2': round((2*I_brake2+(I_diff + k1 * (I_brake2 - I_brake1)))/2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
                 'retom_lv2': round((2*I_brake2-(I_diff + k1 * (I_brake2 - I_brake1)))/2 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
-                'retom_skvoz_hv2': round(I_brake2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
-                'retom_skvoz_lv2': round(I_brake2 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
+                'retom_skvoz_hv2': round((2*I_brake2+(I_diff + k1 * (I_brake2 - I_brake1)))/2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
+                'retom_skvoz_lv2': round((2*I_brake2+(I_diff + k1 * (I_brake2 - I_brake1))) /2/ base['koeff_CT_LV'] * base['I_nom_lv'], 2),
             }
         else:
             return {
                 'retom_hv1': round((2*I_brake1-I_diff)/2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
                 'retom_lv1': round((2*I_brake1+I_diff)/2 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
-                'retom_skvoz_hv1': round(I_brake1 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
-                'retom_skvoz_lv1': round(I_brake1 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
+                'retom_skvoz_hv1' : round((2*I_brake1+I_diff)/2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
+                'retom_skvoz_lv1' : round((2*I_brake1+I_diff)/2 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
                 'retom_hv2': round((2*I_brake2-(I_diff + k1 * (I_brake2 - I_brake1)))/2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
                 'retom_lv2': round((2*I_brake2+(I_diff + k1 * (I_brake2 - I_brake1)))/2 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
-                'retom_skvoz_hv2': round(I_brake2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
-                'retom_skvoz_lv2': round(I_brake2 / base['koeff_CT_LV'] * base['I_nom_lv'], 2),
+                'retom_skvoz_hv2': round((2*I_brake2+(I_diff + k1 * (I_brake2 - I_brake1)))/2 / base['koeff_CT_HV'] * base['I_nom_hv'], 2),
+                'retom_skvoz_lv2': round((2*I_brake2+(I_diff + k1 * (I_brake2 - I_brake1))) /2/ base['koeff_CT_LV'] * base['I_nom_lv'], 2),
             }
 
     def _calculate_arbitrary_retom(self, base, I_brake, I_diff):
         if self.winding_side == WindingSide.HV:
             return {
-                'retom_hv_arb': I_brake * base['I_nom_hv'] / base['koeff_CT_HV'],
-                'retom_lv_arb': (I_brake - I_diff) * base['I_nom_lv'] / base['koeff_CT_LV'],
-                'retom_skvoz_arb_hv': I_brake * base['I_nom_hv'] / base['koeff_CT_HV'],
-                'retom_skvoz_arb_lv': I_brake * base['I_nom_lv'] / base['koeff_CT_LV']
+                'retom_hv_arb': (2*I_brake + I_diff)/2 * base['I_nom_hv'] / base['koeff_CT_HV'],
+                'retom_lv_arb': (2*I_brake - I_diff)/2 * base['I_nom_lv'] / base['koeff_CT_LV'],
+                'retom_skvoz_arb_hv': (2*I_brake + I_diff)/2 * base['I_nom_hv'] / base['koeff_CT_HV'],
+                'retom_skvoz_arb_lv':(2*I_brake + I_diff)/2 * base['I_nom_lv'] / base['koeff_CT_LV'],
             }
         else:
             return {
-                'retom_hv_arb': (I_brake - I_diff) * base['I_nom_hv'] / base['koeff_CT_HV'],
-                'retom_lv_arb': I_brake * base['I_nom_lv'] / base['koeff_CT_LV'],
-                'retom_skvoz_arb_hv': I_brake * base['I_nom_hv'] / base['koeff_CT_HV'],
-                'retom_skvoz_arb_lv': I_brake * base['I_nom_lv'] / base['koeff_CT_LV']
+                'retom_hv_arb': (2*I_brake - I_diff)/2 * base['I_nom_hv'] / base['koeff_CT_HV'],
+                'retom_lv_arb': (2*I_brake + I_diff)/2 * base['I_nom_lv'] / base['koeff_CT_LV'],
+                'retom_skvoz_arb_hv': (2*I_brake + I_diff)/2 * base['I_nom_hv'] / base['koeff_CT_HV'],
+                'retom_skvoz_arb_lv': (2*I_brake + I_diff)/2 * base['I_nom_lv'] / base['koeff_CT_LV'],
             }
 
     def _get_blocking_for_point(self, base, I_brake, is_hv_side, params):
